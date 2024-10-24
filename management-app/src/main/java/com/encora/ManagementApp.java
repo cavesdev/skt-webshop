a56cf76a05d150d67a2938b3199d20a9469b965c
Carlos Cuevas <24899540+cavesdev@users.noreply.github.com>
2024-10-24 12:20:11 -0600

    Fix multi module setup and cleanup (#1)
    * fix pom and rename files
    
    * remove common lib main class

---
+++ management-app/src/main/java/com/encora/ManagementApp.java
@@ -0,0 +1,19 @@
+package com.encora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+    //comment below if deploying outside web container -->
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
